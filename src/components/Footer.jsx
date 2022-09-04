a9c71918fcdea7515057a2f58c4c585bc51c7b78
kohei <kouhei.ikeda@veriserve.co.jp>
2022-09-05 01:27:44 +0900

    フッター修正

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export function Footer() {
     <footer className="py-16">
       <Container className="flex flex-col items-center justify-between md:flex-row">
         {/* <Logo className="h-12 w-auto text-slate-900" /> */}
-        <p className="mt-6 text-base text-slate-500 md:mt-0">
+        <p className="mt-6 text-xs text-slate-500 md:mt-0">
           Copyright &copy; ボクらのホームパーティー制作委員会
         </p>
       </Container>
