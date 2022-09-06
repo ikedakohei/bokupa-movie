cc2f1f7d4577682ecf2653659ab34c39c68d0aab
kohei <kouhei.ikeda@veriserve.co.jp>
2022-09-07 00:58:45 +0900

    Footer修正

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
+import Image from 'next/future/image'
 import { Container } from '@/components/Container'
-// import { Logo } from '@/components/Logo'
+import titleImage from '@/images/bhp_title.png'
 
 export function Footer() {
   return (
     <footer className="py-16">
-      <Container className="flex flex-col items-center justify-between md:flex-row">
-        {/* <Logo className="h-12 w-auto text-slate-900" /> */}
-        <p className="mt-6 text-xs text-slate-500 md:mt-0">
+      <Container className="flex flex-col items-center justify-center md:flex-row">
+        <Image src={titleImage} alt="" className="h-12 w-auto text-slate-900" />
+        <p className="mt-6 text-xs text-slate-500 md:mt-0 md:ml-8">
           Copyright &copy; ボクらのホームパーティー制作委員会
         </p>
       </Container>
