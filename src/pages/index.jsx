0b26d407bbffe5913a62042d7c81628c8bef40fe
kohei <kouhik06@gmail.com>
2022-09-10 07:36:56 +0900

    不要なファイルの削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,11 +3,8 @@ import Head from 'next/head'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Main } from '@/components/Main'
-// import { Newsletter } from '@/components/Newsletter'
-// import { Schedule } from '@/components/Schedule'
 import { Staffs } from '@/components/Staffs'
 import { Casts } from '@/components/Casts'
-// import { Sponsors } from '@/components/Sponsors'
 
 export default function Home() {
   return (
@@ -21,9 +18,6 @@ export default function Home() {
         <Main />
         <Casts />
         <Staffs />
-        {/* <Schedule /> */}
-        {/* <Sponsors /> */}
-        {/* <Newsletter /> */}
       </main>
       <Footer />
     </>
