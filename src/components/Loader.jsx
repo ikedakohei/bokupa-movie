8eeae56eb21fe03c80df037cd2c08da7f2f62c40
kohei <kouhik06@gmail.com>
2022-09-19 12:16:39 +0900

    ローディング画面修正

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ export const Loader = () => {
   return (
     <div style={{zIndex: 10000}} className="sticky top-0 bg-white">
       <div className="flex items-center justify-center h-screen">
-        <div className="animate-spin h-10 w-10 border-4 border-yellow-300 rounded-full border-t-transparent" />
+        <div className="animate-spin h-10 w-10 border-4 border-gray-300 rounded-full border-t-transparent" />
       </div>
     </div>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect (() => {
-    setTimeout(() => {setIsLoading(false)}, 2000);
+    setTimeout(() => {setIsLoading(false)}, 3000);
   }, []);
 
   return (
