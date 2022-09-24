31766e11b2d8b2ff68edf56adca03fb64acd1766
kohei <kouhei.ikeda@veriserve.co.jp>
2022-09-24 19:13:25 +0900

    修正

--- src/components/DirectorMessage.jsx
+++ src/components/DirectorMessage.jsx
@@ -9,7 +9,7 @@ export function DirectorMessage() {
           className="space-y-6 pt-20 font-sans text-lg tracking-tight text-gray-900 md:pt-32"
         >
           <div className="flex justify-center">
-            <h2 className="mb-4 px-2 pb-2 font-kaisei text-3xl font-bold tracking-tighter text-gray-900">
+            <h2 className="mb-4 px-2 pb-2 font-kaisei text-3xl tracking-tighter text-gray-900">
               監督よりご挨拶
             </h2>
           </div>
