e4c0c646c587eb27ee9733fa7b4146986694ded8
kohei <kouhei.ikeda@veriserve.co.jp>
2022-09-25 02:30:04 +0900

    修正

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import titleImage from '@/images/bhp_title.webp'
 
 export function Footer() {
   return (
-    <footer className="py-16">
+    <footer className="pt-16 pb-8">
       <Container className="flex flex-col items-center justify-center md:flex-row">
         <Image src={titleImage} alt="" className="h-12 w-auto text-slate-900" />
         <p className="mt-6 font-sans text-xs text-slate-500 md:mt-0 md:ml-8">
