18c186eac502b9e94cd2e6e7cbbb3672aca3c6fb
kohei <kouhei.ikeda@veriserve.co.jp>
2022-09-29 01:33:51 +0900

    修正

--- src/pages/campaigns.jsx
+++ src/pages/campaigns.jsx
@@ -1,6 +1,17 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import { Container } from '@/components/Container'
 import { Header } from '@/components/Header'
+
+const shops = [
+  // {
+  //   place: '東京・新宿二丁目',
+  //   name: 'テスト',
+  //   link: '#',
+  //   date: '11/19（土）～',
+  //   note: 'ゲイオンリー',
+  // },
+]
 
 export default function Campaigns() {
   return (
@@ -24,7 +35,62 @@ export default function Campaigns() {
                     キャンペーン
                   </h2>
                 </div>
-                <p className="text-center text-2xl">作成中...</p>
+                <div className="px-4 sm:px-6 lg:px-8">
+                  <div className="mt-8 flex flex-col">
+                    <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                      <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                        <div className="overflow-hidden ring-1 ring-black ring-opacity-5 md:rounded">
+                          <table className="min-w-full divide-y divide-gray-500 font-sans">
+                            <thead className="bg-neutral-500">
+                              <tr>
+                                <th
+                                  scope="col"
+                                  className="py-3.5 pl-4 pr-3 text-left text-sm font-normal text-white sm:pl-6"
+                                >
+                                  地域
+                                </th>
+                                <th
+                                  scope="col"
+                                  className="px-3 py-3.5 text-left text-sm font-normal text-white"
+                                >
+                                  店名
+                                </th>
+                                <th
+                                  scope="col"
+                                  className="px-3 py-3.5 text-left text-sm font-normal text-white"
+                                >
+                                  備考
+                                </th>
+                              </tr>
+                            </thead>
+                            <tbody className="divide-y divide-gray-400 bg-white">
+                              {shops.map((shop) => (
+                                <tr key={shop.name}>
+                                  <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-900 sm:pl-6">
+                                    {shop.place}
+                                  </td>
+                                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900">
+                                    <Link
+                                      href={shop.link}
+                                      target="_blank"
+                                      rel="noopener noreferrer"
+                                      className="underline"
+                                    >
+                                      {shop.name}
+                                    </Link>
+                                  </td>
+                                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900">
+                                    {shop.note}
+                                  </td>
+                                </tr>
+                              ))}
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </Container>
