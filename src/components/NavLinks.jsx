dcdc7df6de7d1b6d4ffffcb4f73e2cd3ed1d8762
kohei <kouhik06@gmail.com>
2022-10-02 00:40:13 +0900

    修正

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -16,8 +16,8 @@ export function NavLinks() {
     <Link
       key={label}
       href={href}
-      target={label === 'NEWS' && '_blank'}
-      rel={label === 'NEWS' && 'noopener noreferrer'}
+      target={label === 'NEWS' ? '_blank' : undefined}
+      rel={label === 'NEWS' ? 'noopener noreferrer' : undefined}
       className="relative -my-2 -mx-3 rounded-lg px-3 py-2 text-gray-700 transition-colors delay-150 hover:text-gray-900 hover:delay-[0ms]"
       onMouseEnter={() => setHoveredIndex(index)}
       onMouseLeave={() => setHoveredIndex(null)}
