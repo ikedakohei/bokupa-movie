ec0eeec202d304d03f8c6f67bc3ae1e8209703ac
kohei <kouhei.ikeda@veriserve.co.jp>
2023-03-08 23:00:06 +0900

    修正

--- src/components/Twitter.jsx
+++ src/components/Twitter.jsx
@@ -3,12 +3,22 @@ import { Container } from '@/components/Container'
 export function Twitter() {
   return (
     <Container className="relative">
-      <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
+      <div className="flex justify-center">
+        <h2 className="mt-20 font-kaisei text-4xl tracking-tighter text-gray-900 md:mt-32">
+          SNS
+        </h2>
+      </div>
+      <div className="mx-auto mt-12 max-w-2xl lg:max-w-4xl lg:px-12">
         <div
           id="twitter"
-          className="space-y-6 pt-20 font-sans text-lg tracking-tight text-gray-900 md:pt-32"
+          className="space-y-6 font-sans text-lg tracking-tight text-gray-900"
         >
-          <a className="twitter-timeline" data-lang="ja" data-theme="light" href="https://twitter.com/BokupaM?ref_src=twsrc%5Etfw" />
+          <a
+            className="twitter-timeline"
+            data-lang="ja"
+            data-theme="light"
+            href="https://twitter.com/BokupaM?ref_src=twsrc%5Etfw"
+          />
         </div>
       </div>
     </Container>
